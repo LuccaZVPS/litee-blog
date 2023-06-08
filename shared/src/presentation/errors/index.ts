9b6196d83a89a6aae265cdd8499264e5c6b0e645
Lucca <luccazvps@gmail.com>
2023-06-08 19:08:22 -0300

    feat: add controllers interfaces

---
+++ shared/src/presentation/errors/index.ts
@@ -0,0 +1,6 @@
+export * from "./any-http-error";
+export * from "./bad-request-error";
+export * from "./base-error";
+export * from "./forbiden-error";
+export * from "./not-found-error";
+export * from "./unauthorized-error";
