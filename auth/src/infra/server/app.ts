6305bc0844becbcc66274bb57fa842612ce4ecd5
Lucca <luccazvps@gmail.com>
2023-06-09 14:05:31 -0300

    chore: add express-async-errors

--- auth/src/infra/server/app.ts
+++ auth/src/infra/server/app.ts
@@ -1,6 +1,9 @@
 import express, { json } from "express";
+import { errorHandler } from "@litee-blog/shared/infra/express";
 import { accountRoutes } from "./routes/account.routes";
+require("express-async-errors");
 const app = express();
 app.use(json());
 app.use("/api/auth", accountRoutes);
+app.use(errorHandler);
 export { app };
