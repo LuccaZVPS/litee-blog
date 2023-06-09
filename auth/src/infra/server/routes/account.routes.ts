9f7200382dff90368757140a6d8620c0d841868a
Lucca <luccazvps@gmail.com>
2023-06-09 15:00:31 -0300

    feat: add account verification route

--- auth/src/infra/server/routes/account.routes.ts
+++ auth/src/infra/server/routes/account.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import { adaptRoute } from "@litee-blog/shared/infra/express";
 import { makeSignUpController } from "../../../main/factories/controllers/signup-controller-factory";
+import { makeVerifyAccount } from "../../../main/factories/useCases/verify-account-factory";
+import { makeVerifyAccountController } from "../../../main/factories/controllers/verify-account-controller-factory";
 export const accountRoutes = Router();
 accountRoutes.post("/signup", adaptRoute(makeSignUpController()));
+accountRoutes.post(
+  "/verify/:id/:secret",
+  adaptRoute(makeVerifyAccountController())
+);
