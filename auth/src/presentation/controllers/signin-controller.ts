cf17a6d49784bd05621f77f2ee0c1cd4677d0e56
Lucca <luccazvps@gmail.com>
2023-06-10 16:53:41 -0300

    chore: add jsonwebtoken

--- auth/src/presentation/controllers/signin-controller.ts
+++ auth/src/presentation/controllers/signin-controller.ts
@@ -2,17 +2,21 @@ import {
   IController,
   IResponse,
 } from "@litee-blog/shared/presentation/protocols";
-import { ISignIn } from "../domain/useCases/signin";
+import { ISignIn } from "../../domain/useCases/signin";
 import { Unauthorized } from "@litee-blog/shared/presentation/errors";
 
 export class SignInController implements IController {
   constructor(private readonly authentication: ISignIn) {}
   async handle(req: any): Promise<IResponse> {
     const { email, password } = req;
-    const account = await this.authentication.signIn(email, password);
-    if (!account) {
+    const jwtToken = await this.authentication.signIn(email, password);
+    if (!jwtToken) {
       throw new Unauthorized("Unauthrozized");
     }
-    return { status: 200, body: "Logged in!" };
+    const JwtCookie = {
+      name: "jwt",
+      value: jwtToken,
+    };
+    return { status: 200, body: "Logged in!", cookies: [JwtCookie] };
   }
 }
