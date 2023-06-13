a6da6fa4fe6ffcf4f8018e55f58da01bcf9dfc8d
Lucca <luccazvps@gmail.com>
2023-06-13 08:43:43 -0300

    feat: add auth miidleware

--- shared/src/infra/express/express-middleware.ts
+++ shared/src/infra/express/express-middleware.ts
@@ -1,15 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import { IMiddleware } from "../../presentation/protocols/middleware";
 import { middlewareValidator } from "../../presentation/middlewares";
+import { authMiddleware } from "../../presentation/middlewares/auth-middleware";
 
 export const adaptMiddeware = (middleware: (dto: any) => Promise<void>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const request = {
       ...(req.body || {}),
       ...(req.params || {}),
-      userId: req.userId,
+      userId: req.accountId,
     };
     await middleware(request);
     next();
   };
 };
+export const validateBodyMiddleware = (dto: any) =>
+  adaptMiddeware(middlewareValidator.handle(dto));
+export const authorizedMiddleware = () => adaptMiddeware(authMiddleware.handle);
--- shared/src/infra/express/express-route.ts
+++ shared/src/infra/express/express-route.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from "express";
 import { IController } from "../../presentation/protocols";
-
 export const adaptRoute = (controller: IController) => {
   return async (req: Request, res: Response) => {
     const request = {
       ...(req.body || {}),
       ...(req.params || {}),
-      userId: req.userId,
+      userId: req.accountId,
     };
     const httpResponse = await controller.handle(request);
     httpResponse.cookies?.forEach((c) => {
