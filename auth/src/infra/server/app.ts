db112cc254c3c9cedc1f4cf6cab71b0d2c649fc4
Lucca <luccazvps@gmail.com>
2023-06-23 09:18:51 -0300

    feat: add text size for content field on database

--- auth/src/infra/server/app.ts
+++ auth/src/infra/server/app.ts
@@ -2,8 +2,16 @@ require("express-async-errors");
 import express, { json } from "express";
 import { errorHandler } from "@litee-blog/shared/infra/express";
 import { accountRoutes } from "./routes/account.routes";
+import cookieSession from "cookie-session";
 const app = express();
+
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 app.use("/api/auth", accountRoutes);
 app.use(errorHandler);
 export { app };
--- posts/src/infra/server/app.ts
+++ posts/src/infra/server/app.ts
@@ -4,9 +4,17 @@ import cookieParser from "cookie-parser";
 import { router } from "./routes/post-routes";
 import { errorHandler } from "@litee-blog/shared/infra/express";
 import { config } from "dotenv";
+import cookieSession from "cookie-session";
 config();
 const app = express();
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+    sameSite: true,
+  })
+);
 app.use(cookieParser());
 app.use("/api/posts", router);
 app.use(errorHandler);
