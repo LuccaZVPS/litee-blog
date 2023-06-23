23ab1b826f419d524c4a34ef00fe64b72f826717
Lucca <luccazvps@gmail.com>
2023-06-23 13:26:50 -0300

    feat: add new event types

--- shared/src/infra/broker/events.ts
+++ shared/src/infra/broker/events.ts
@@ -1,11 +1,22 @@
 export enum EventNames {
   AccountCreated = "account:created",
-  AccountVerified = "account:verified",
-  BlogCreated = "blog:created",
+  CategoryCreated = "category:created",
+  CategoryDeleted = "category:deleted",
 }
 export interface AccountCreated {
   _id: string;
   name: string;
   email: string;
   secret: string;
+}
+export interface CategoryCreated {
+  id: string;
+  title: string;
+}
+export interface CategoryDeleted {
+  id: string;
+}
+export interface CategoryUpdated {
+  id: string;
+  title: string;
 }
