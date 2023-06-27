4bcef86248b6724d368b3744f53aec84b9df0896
Lucca <luccazvps@gmail.com>
2023-06-27 08:23:39 -0300

    fix: change wrong use cases implementation

--- categories/src/presentation/controllers/add-category-controller.ts
+++ categories/src/presentation/controllers/add-category-controller.ts
@@ -4,22 +4,15 @@ import {
 } from "@litee-blog/shared/presentation/protocols";
 import { IAddCategory } from "../../domain/useCases/add-category";
 import { AddCategoryDTO } from "../DTOs/add-category-dto";
-import fs from "fs";
 import { BadRequestError } from "@litee-blog/shared/presentation";
 export class AddCategoryController implements IController {
   constructor(private readonly addCategory: IAddCategory) {}
   async handle(req: AddCategoryDTO): Promise<IResponse> {
-    // eslint-disable-next-line no-useless-catch
-    try {
-      const errors = req.validationErrors;
-      if (errors && errors.length > 0) {
-        throw new BadRequestError(errors);
-      }
-      const { _id } = await this.addCategory.add(req.title, req.file.filename);
-      return { status: 201, body: { id: _id } };
-    } catch (e) {
-      fs.unlinkSync(req.file.filename);
-      throw e;
+    const errors = req.validationErrors;
+    if (errors && errors.length > 0) {
+      throw new BadRequestError(errors);
     }
+    const { _id } = await this.addCategory.add(req.title, req.file.filename);
+    return { status: 201, body: { id: _id } };
   }
 }
--- posts/src/presentation/controllers/add-post-controller.ts
+++ posts/src/presentation/controllers/add-post-controller.ts
@@ -1,34 +1,27 @@
-/* eslint-disable no-useless-catch */
 import {
   IController,
   IResponse,
 } from "@litee-blog/shared/presentation/protocols";
 import { AddPostDTO } from "../DTOs/add-post-dto";
 import { IAddPost } from "../../domnain/useCases/add-post";
-import fs from "fs";
 import { BadRequestError } from "@litee-blog/shared/presentation";
 export class AddPostController implements IController {
   constructor(private readonly addPost: IAddPost) {}
   async handle(req: AddPostDTO): Promise<IResponse> {
-    try {
-      const errors = req.validationErrors;
-      if (errors && errors.length > 0) {
-        throw new BadRequestError(errors);
-      }
-      const { title, content, categories, accountId, file } = req;
-      const imageNameFields = file.filename.split("/");
-      const post = await this.addPost.add({
-        title,
-        categories,
-        content,
-        accountId,
-        imageName: imageNameFields[imageNameFields.length - 1],
-        imagePath: file.filename,
-      });
-      return { status: 201, body: { id: post.id } };
-    } catch (e: unknown) {
-      fs.unlinkSync(req.file.filename);
-      throw e;
+    const errors = req.validationErrors;
+    if (errors && errors.length > 0) {
+      throw new BadRequestError(errors);
     }
+    const { title, content, categories, accountId, file } = req;
+    const imageNameFields = file.filename.split("/");
+    const post = await this.addPost.add({
+      title,
+      categories,
+      content,
+      accountId,
+      imageName: imageNameFields[imageNameFields.length - 1],
+      imagePath: file.filename,
+    });
+    return { status: 201, body: { id: post.id } };
   }
 }
